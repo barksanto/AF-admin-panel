35f5453d904d1f50c458b12bc131c6c9db0eea4e
Barkley <barkley.santo@gmail.com>
2022-06-22 15:34:23 +0300

    first iteration of navbar styling done

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,17 @@
 import "./sidebar.scss"
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PeopleIcon from '@mui/icons-material/People';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
+// import RoomPreferencesIcon from '@mui/icons-material/RoomPreferences'; for hangout
 
 const Sidebar = () => {
   return (
@@ -9,21 +22,68 @@ const Sidebar = () => {
       <hr />
       <div className="center">
         <ul>
+          <p className="title">MAIN</p>
           <li>
+            <DashboardIcon className="icon"/>
             <span>Dashboard</span>
           </li>
+          <p className="title">LISTS</p>
           <li>
-            <span>Dashboard</span>
+            <PeopleIcon className="icon"/>
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <InventoryIcon className="icon"/>
+            <span>Products</span>
+            {/* <RoomPreferencesIcon className="icon"/> */}
+            {/* <span>Hangouts</span>  to be Hangs Management */}
           </li>
           <li>
-            <span>Dashboard</span>
+            <ReceiptIcon className="icon"/>
+            <span>Orders</span>
           </li>
+          <li>
+            <LocalShippingIcon className="icon"/>
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <AnalyticsIcon className="icon"/>
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsActiveIcon className="icon"/>
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <HealthAndSafetyIcon className="icon"/>
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyIcon className="icon"/>
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon className="icon"/>
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleIcon className="icon"/>
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon className="icon"/>
+            <span>Logout</span>
+          </li>
+
         </ul>
       </div>
-      <div className="bottom">color options</div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
     </div>
   )
 }

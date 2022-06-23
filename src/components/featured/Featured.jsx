d480ae28d720bfd031cc3c67b50cfe68e2bc74b3
Barkley <barkley.santo@gmail.com>
2022-06-23 16:10:58 +0300

    added linear gradient area char with responsive container

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,19 @@
 import "./featured.scss"
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 function Featured() {
   return (
-    <div className="featured">Featured</div>
+    <div className="featured">
+      <div className="top">
+        <h1>Total Revenue</h1>
+        <MoreVertIcon fontSize="small"/>
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
